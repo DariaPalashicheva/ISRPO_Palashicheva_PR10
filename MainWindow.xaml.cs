865a3f6fe92c4230a3262ff16456790eeed5dc5b
palashichevaa@mail.ru <palashichevaa@mail.ru>
2022-10-24 12:07:07 +1000

    фикс1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISRPO_Palashicheva_PR10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            listbox1.Items.Add(textbox1.Text);
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem li = (ListBoxItem)listbox1.Items[listbox1.SelectedIndex];
+            listbox1.Items.Remove(li);
+            // или так listbox1.Items.Remove(listbox1.SelectedItem);
+
+        }
+    }
+}
